3923728ce64d90da9b5d03d696299151f5caacda
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-09-13 18:01:34 -0400

    changed case statement to if's for betty line count

--- error_handlers.c
+++ error_handlers.c
@@ -8,45 +8,29 @@ void exit_error(unsigned int id, ...)
 	va_list args;
 
 	va_start(args, id);
-	switch (id)
-	{
-		case ERR_ARGCOUNT:
-			fprintf(stderr, "USAGE: monty file\n");
-			break;
-		case ERR_FOPEN:
-			fprintf(stderr, "Error: Can't open file %s\n", va_arg(args, char *));
-			break;
-		case ERR_MALLOC:
-			fprintf(stderr, "Error: malloc failed\n");
-			break;
-		case ERR_INSTRUCTION:
-			va_arg(args, unsigned int);
-			fprintf(stderr, "L%u: unknown instruction %s\n",
-					va_arg(args, unsigned int), va_arg(args, char *));
-			break;
-		case ERR_PUSH:
-			fprintf(stderr, "L%u: usage: push integer\n", va_arg(args, unsigned int));
-			break;
-		case ERR_PINT:
-			fprintf(stderr, "L%u: can't pint, stack empty\n",
+	if (id == ERR_ARGCOUNT)
+		fprintf(stderr, "USAGE: monty file\n");
+	else if (id == ERR_FOPEN)
+		fprintf(stderr, "Error: Can't open file %s\n", va_arg(args, char *));
+	else if (id == ERR_MALLOC)
+		fprintf(stderr, "Error: malloc failed\n");
+	else if (id == ERR_INSTRUCTION)
+		fprintf(stderr, "L%u: unknown instruction %s\n",
+				va_arg(args, unsigned int), va_arg(args, char *));
+	else if (id == ERR_PUSH)
+		fprintf(stderr, "L%u: usage: push integer\n", va_arg(args, unsigned int));
+	else if (id == ERR_PINT)
+		fprintf(stderr, "L%u: can't pint, stack empty\n",
 					va_arg(args, unsigned int));
-			break;
-		case ERR_POP:
-			fprintf(stderr, "L%u: can't pop an empty stack\n",
+	else if (id == ERR_POP)
+		fprintf(stderr, "L%u: can't pop an empty stack\n",
 					va_arg(args, unsigned int));
-			break;
-		case ERR_SWAP:
-			fprintf(stderr, "L%u: can't swap, stack too short\n",
+	else if (id == ERR_SWAP)
+		fprintf(stderr, "L%u: can't swap, stack too short\n",
 					va_arg(args, unsigned int));
-			break;
-		case ERR_ADD:
-			fprintf(stderr, "L%u: can't add, stack too short\n",
+	else if (id == ERR_ADD)
+		fprintf(stderr, "L%u: can't add, stack too short\n",
 					va_arg(args, unsigned int));
-			break;
-		default:
-			break;
-	}
-
 	va_end(args);
 	destroy_nodes();
 	exit(EXIT_FAILURE);
